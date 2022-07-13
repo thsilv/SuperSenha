ada25b4b6860e91b13d35944e74b83d592eadde9
thsilv <t.silvaemp@gmail.com>
2022-07-13 14:05:19 -0300

    Initial Commit

---
+++ SuperSenha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperSenha
+//
+//  Created by Thiago Silva on 13/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
